baa5b81d770f6a4f0ff3a8814c1a1fe64f1e6d6b
hanifgibran <sourcegibran@gmail.com>
2024-08-07 14:00:22 +0700

    tugas no3

--- tugas1.js
+++ tugas1.js
@@ -3,3 +3,8 @@ const variableConst = "Ini variabel const";
 
 const arrowFunction = (param1, param2) => param1 + param2;
 console.log(arrowFunction(5, 3));
+
+let name = "John";
+let age = 25;
+let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting);
