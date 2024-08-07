2aa65d16277a8a30eedeee5d8b3b39a9ddaba5ef
hanifgibran <sourcegibran@gmail.com>
2024-08-07 14:04:14 +0700

    tugas no6

---
+++ main.js
@@ -0,0 +1,4 @@
+import { add, multiply } from './module.js';
+
+console.log(add(2, 3)); 
+console.log(multiply(2, 3));
--- tugas1.js
+++ tugas1.js
@@ -19,3 +19,7 @@ let array1 = [1, 2, 3];
 let array2 = [4, 5, 6];
 let combinedArray = [...array1, ...array2];
 console.log(combinedArray);
+let obj1 = { a: 1, b: 2 };
+let obj2 = { c: 3, d: 4 };
+let combinedObj = { ...obj1, ...obj2 };
+console.log(combinedObj);
