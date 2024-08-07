cadf74a78eebbd8dcd14005f4a5b4eba350d919d
hanifgibran <sourcegibran@gmail.com>
2024-08-07 14:05:42 +0700

    tugas no8

--- tugas1.js
+++ tugas1.js
@@ -33,3 +33,38 @@ const promiseFunction = () => {
 };
 
 promiseFunction().then(result => console.log(result)); 
+
+const asyncFunction = async () => {
+    let result = await promiseFunction();
+    console.log(result); 
+};
+
+asyncFunction();
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+let person1 = new Person("Alice", 28);
+person1.greet();
+class Employee extends Person {
+    constructor(name, age, job) {
+        super(name, age);
+        this.job = job;
+    }
+
+    work() {
+        console.log(`${this.name} is working as a ${this.job}.`);
+    }
+}
+
+let employee1 = new Employee("Bob", 35, "Developer");
+employee1.greet();
+employee1.work()
